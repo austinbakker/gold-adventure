31a37afd20a3c611387b2b84331db20cfe4ec551
austinbakker <austin@austinbakker.com>
2020-12-24 11:01:25 +0100

    Create textinput.vue

---
+++ components/textinput.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="text" name="" id="">
+  </div>
+</template>
+
+
+<script lang='ts' >
+export default {
+ props: [],
+ components: {},
+ setup() {
+   
+   
+ }
+}
+
+
+</script>
+
+
+<style lang='scss' module>
+ 
+ 
+//POWERED BY AUSTIN
+</style>
+ 
+ 
